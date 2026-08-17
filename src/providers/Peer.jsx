71e9db11a01ef2000acd067af1256ca19b728ec9
Mohit46200 <mohit22600@gmail.com>
2026-08-17 20:41:03 +0530

    Fix React Router deployment

--- src/providers/Peer.jsx
+++ src/providers/Peer.jsx
@@ -11,7 +11,7 @@ export const PeerProvider = (props) => {
 // Create RTCPeerConnection
 //         ↓
 // createOffer()
-//         ↓
+//         ↓                                
 // setLocalDescription()
 //         ↓
 // Browser contacts STUN server
